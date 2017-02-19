38f05773e1f477d496e78fbd8790cdf442633cce
samover <samuel.overloop@gmail.com>
2017-02-19 10:40:17 +0100

    Insert user in db in unit test

--- test/services/auth.services_test.js
+++ test/services/auth.services_test.js
@@ -9,14 +9,18 @@
 'use strict';
 
 const authService = require('../../src/services/auth.service');
+const User = require('../../src/models/user.model');
 
-const testUser = {
+const testUser = new User({
     name: 'sam',
     password: 'test',
     token: 'access_token',
-};
+});
 
 describe('services/auth.service', () => {
+    before(() => testUser.save());
+    after(() => testUser.remove());
+
     describe('#login', () => {
         it('returns the user\'s token', () => {
             return authService.login(testUser.name, testUser.password)
